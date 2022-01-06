7dfb0706519b9c60df4455f9da77756b2b4a449f
Alex Lin <hs9021401@gmail.com>
2022-01-06 10:47:12 +0800

    Add some styling

---
+++ scripts/test.js
@@ -0,0 +1,13 @@
+document.querySelector('.link-home').addEventListener('click', (e) => {
+    console.log('Home');    
+});
+
+document.querySelector('.link-news').addEventListener('click', (e) => {
+    console.log('News');    
+});
+document.querySelector('.link-repair').addEventListener('click', (e) => {
+    console.log('Repair');    
+});
+document.querySelector('.link-aboutus').addEventListener('click', (e) => {
+    console.log('About Us');
+});
